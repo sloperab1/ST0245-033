df907c0a57b2eb029de66985ab97958777885d93
sloperab1 <60229746+sloperab1@users.noreply.github.com>
2020-04-26 15:47:37 -0500

    Add files via upload

---
+++ laboratorios/lab04/codigo/Lab4.java
@@ -0,0 +1,61 @@
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+
+public class Lab4 {
+
+
+	public static void main(String[] args) throws Exception 
+		{ 
+		    // pass the path to the file as a parameter 
+		    File file = 
+		      new File("C:\\Users\\Usuario\\Desktop\\Ejemplito.txt"); 
+		    Scanner sc = new Scanner(file); 
+		  
+		    while (sc.hasNextLine()) 
+		      System.out.println(sc.nextLine()); 
+		  
+		} 
+		public void searchByAuthor(file.nextLine, String a){
+	        if(n == a) {
+	        	System.out.println("");
+	        }
+	    }
+	    
+	    public ArrayList<String> searchBySize(Tree n, double s){
+	       
+	    }
+	    
+	    public void printFiles(NodeDirectory a){
+	        
+	    }
+	    @Override
+		public String toString() {
+			return "Lab4 []";
+		}
+		//using guideline from
+	//https://stackoverflow.com/questions/3522454/how-to-implement-a-tree-data-structure-in-java
+		public class Tree<T> {
+	    private Node<T> root;
+
+	    public Tree(T rootData) {
+	        root = new Node<T>();
+	        root.data = rootData;
+	        root.children = new ArrayList<Node<T>>();
+	    }
+
+	    public  class Node<T> {
+	        private T data;
+	        private Node<T> parent;
+	        private List<Node<T>> children;
+	    }
+	    
+	    
+		}
+}
+	
+	
+
+
