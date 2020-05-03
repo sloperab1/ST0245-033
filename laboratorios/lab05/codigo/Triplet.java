8d69058f2edc4caaef8b8ccdfdaf928bb4362957
jjescobarb1 <60229787+jjescobarb1@users.noreply.github.com>
2020-05-03 16:20:47 -0500

    Add files via upload

--- laboratorios/lab05/codigo/Triplet.java
+++ laboratorios/lab05/codigo/Triplet.java
@@ -1,4 +1,10 @@
-
-public class Triplet {
-
-}
+public class Triplet<X, Y, Z> { 
+  public final X x; 
+  public final Y y; 
+  public final Z z;
+  public Triplet(X x, Y y, Z z) { 
+    this.x = x; 
+    this.y = y; 
+    this.z = z;
+  } 
+} 
